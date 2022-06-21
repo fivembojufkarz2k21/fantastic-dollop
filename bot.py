d8a52c969f1767525ec80da7654b96f3499f53c1
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-06-21 15:53:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ while a==True:
 
 
 
-driver.get('https://replit.com/@gdfhdfg/InnocentUnderstatedRedundantcode?v=1')
+driver.get('https://replit.com/@dsfjoh/LividUntrueInterface?v=1')
 driver.execute_script("""function getElementByXpath(path){return document.evaluate(path,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue}getElementByXpath("/html/body/div[1]/div/main/div[3]/div/div/div[2]/div/button[1]").click();""")
 time.sleep(55)
 
