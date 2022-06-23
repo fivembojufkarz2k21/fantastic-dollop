16436f33c8b776780181434f32a261583eec6cb8
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-06-23 04:57:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ options.user_data_dir = "rawr"
 options.add_argument("--window-size=1920,1080")
 options.add_argument('--user-data-dir=rawr')
 options.add_argument("--remote-debugging-port=38223")
-driver = uc.Chrome(options=options)  # version_main allows to specify your chrome version instead of following chrome global version
+driver = uc.Chrome(options=options, version_main=102)  # version_main allows to specify your chrome version instead of following chrome global version
 driver.set_window_size(1920, 1080)
 
 print("rawr")
