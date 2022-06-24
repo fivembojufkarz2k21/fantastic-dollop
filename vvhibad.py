cf30ad906f6c7188bc8f7df72466ade8b0993370
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-06-24 23:11:00 +0200

    Create vvhibad.py

---
+++ vvhibad.py
@@ -0,0 +1,65 @@
+import undetected_chromedriver as uc
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+from selenium.webdriver.common.by import By
+import json
+import os
+import requests
+from requests.structures import CaseInsensitiveDict
+#import clipboard
+options = uc.ChromeOptions()
+
+# with open("proxy.txt") as f:
+#     lines = f.readlines()
+# PROXY = random.choice(lines)
+
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+
+passwordrepl = "rawr12!AAc"
+
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+options.user_data_dir = "rawr"
+options.add_argument('--user-data-dir=rawr')
+driver = uc.Chrome(options=options)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.get("https://client.falixnodes.net/auth/register")
+time.sleep(3)
+emailrepl = "".join(random.sample(username_for, long_username))+"@cldkid.com"
+xd = False
+frime = driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/center/div/iframe")
+driver.switch_to.frame(frime)
+timez = int(time.time())+300
+while xd == False:
+    time.sleep(1)
+    print(driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/div[1]").get_attribute("aria-checked"))
+    if(timez<int(time.time())):
+        cockz
+    if(driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div/div/div[1]").get_attribute("aria-checked")=="false"):
+        xd = False
+    else:
+        xd = True
+driver.switch_to.default_content()
+time.sleep(0.5)
+driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/div[1]/input").send_keys(emailrepl)
+time.sleep(0.1)
+driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/div[2]/div[1]/input").send_keys("Cloudkid12!")
+time.sleep(0.1)
+driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/div[2]/div[2]/input").send_keys("Cloudkid12!")
+driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/div[3]/input").click()
+time.sleep(1)
+driver.find_element(By.XPATH, "/html/body/main/div/div/div/div/div/div/div[2]/div/form/div[4]/button").click()
+time.sleep(6)
+driver.get("https://client.falixnodes.net/create")
+driver.find_element(By.XPATH, "/html/body/main/div/div/div[4]/div/div/div[2]/div/form/div[1]/input").send_keys("let go")
+driver.find_element(By.XPATH, "/html/body/main/div/div/div[4]/div/div/div[2]/div/form/div[3]/input").click()
+time.sleep(3)
+driver.find_element(By.XPATH, "/html/body/main/div/div/div[4]/div/div/div[2]/div/form/input").click()
