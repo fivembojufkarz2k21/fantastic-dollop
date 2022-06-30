2f5c11e007dc8591e7ce38165cd56e6127c2e5d3
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-06-30 13:16:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ username_for = low_word
 password_for = low_word + upper_word + number + symbols
 long_password = 16
 long_username = 12
+long_usernamez = 18
 #xdr = "sendelp"
 headers = {
     'cache-control': "no-cache",
@@ -42,7 +43,7 @@ driver.set_window_size(1920, 1080)
 print("rawr")
 
 usernamerepl = "".join(random.sample(username_for, long_username))
-emailrepl = "".join(random.sample(username_for, long_username))+"@cloudkid.com"
+emailrepl = "".join(random.sample(username_for, long_username))+"@gmail.com"
 a = True
 az = True
 time.sleep(3)
