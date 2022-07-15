ab5b7df230967d9900a054a4c7ed8b9e1818a8c0
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-15 18:13:16 +0200

    Create idfklol.py

---
+++ idfklol.py
@@ -0,0 +1,38 @@
+import undetected_chromedriver as uc
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+import json
+import os
+import requests
+from selenium.webdriver.common.by import By
+options = uc.ChromeOptions()
+
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+long_usernamez = 18
+#xdr = "sendelp"
+
+
+passwordrepl = "rawr12!AAc"
+
+
+
+
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+driver.get("https://www.youtube.com/watch?v=grdP213KD-Y")
+time.sleep(9)
+driver.find_element(By.XPATH, '/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/a').click()
+time.sleep(69420)
