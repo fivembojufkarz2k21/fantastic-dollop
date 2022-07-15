d0f846c0b935b69d7cdfab95bd1076d69a5ac0f1
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-15 19:10:54 +0200

    Update idfklol.py

--- idfklol.py
+++ idfklol.py
@@ -1,39 +1,40 @@
-if __name__ ==  '__main__':
-  import undetected_chromedriver as uc
-  from selenium.webdriver import Chrome
-  from selenium import webdriver
-  from selenium.webdriver.common.keys import Keys
-  import time
-  import random
-  import json
-  import os
-  import requests
-  from selenium.webdriver.common.by import By
-  options = uc.ChromeOptions()
+import undetected_chromedriver as uc
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+import json
+import os
+import requests
+from selenium.webdriver.common.by import By
+options = uc.ChromeOptions()
 
-  low_word = "abcdefghijklkmnopqrstuvwxyz"
-  upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
-  number = "1234567890"
-  symbols = "!@#$%&*"
-  username_for = low_word
-  password_for = low_word + upper_word + number + symbols
-  long_password = 16
-  long_username = 12
-  long_usernamez = 18
-  #xdr = "sendelp"
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+long_usernamez = 18
+#xdr = "sendelp"
 
 
-  passwordrepl = "rawr12!AAc"
-
+passwordrepl = "rawr12!AAc"
 
 
 
-  options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
-  options.add_argument("--window-size=1920,1080")
-  options.add_argument("--remote-debugging-port=38223")
-  driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
-  driver.set_window_size(1920, 1080)
-  driver.get("https://www.youtube.com/watch?v=grdP213KD-Y")
-  time.sleep(9)
-  #driver.find_element(By.XPATH, '/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/a').click()
-  time.sleep(69420)
+
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--user-data-dir=vv')
+options.user_data_dir = "vv"
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+driver.get("https://www.youtube.com/watch?v=grdP213KD-Y")
+time.sleep(9)
+driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[31]/div[2]/div[1]/button').click()
+time.sleep(69420)
