b73efba1e9041f0fc9ace6fd6a77dc90d1a8f9c3
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-16 20:25:52 +0200

    Update idkwhatamdoing.py

--- idkwhatamdoing.py
+++ idkwhatamdoing.py
@@ -7,8 +7,6 @@ long_username = 12
 
 while True:
     usernamerepl = "".join(random.sample(username_for, long_username))
-    os.system("fallocate -l 100M "+usernamerepl)
-    time.sleep(1)
+    os.system("fallocate -l 10M "+usernamerepl)
     os.system("curl --form myFile='@"+usernamerepl+"' https://cdn.dnxrg.net/api/upload")
-    time.sleep(1)
     os.system("rm "+usernamerepl)
