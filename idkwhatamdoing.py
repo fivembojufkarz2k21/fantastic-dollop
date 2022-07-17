5b3e1570397e72e44966e945ec2a9350a5b1a3ab
fivembojufkarz2k21 <107816075+fivembojufkarz2k21@users.noreply.github.com>
2022-07-17 19:49:15 +0200

    Update idkwhatamdoing.py

--- idkwhatamdoing.py
+++ idkwhatamdoing.py
@@ -15,16 +15,8 @@ def rawr():
         
 thread1 = threading.Thread(target=rawr)
 thread2 = threading.Thread(target=rawr)
-thread3 = threading.Thread(target=rawr)
-thread4 = threading.Thread(target=rawr)
-thread5 = threading.Thread(target=rawr)
-thread6 = threading.Thread(target=rawr)
 thread1.run()
 thread2.run()
-thread3.run()
-thread4.run()
-thread5.run()
-thread6.run()
 while True:
     time.sleep(10)
     print("up")
